1f7d039f4ae609e3f9ee3787d6f6572c79be124a
Andre Hattingh <andre@incomar.com>
2019-03-01 13:22:44 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ZCM.jl
 Julia package for ZCM
+
+This is a module for ZCM in Julia 1.0. The original source code was found [here](https://github.com/ZeroCM/zcm/tree/master/zcm/julia) and then modified for Julia 1.0 from Julia 0.6.
+
+To install ZCM, please follow the instructions at the official [ZCM repository](https://github.com/ZeroCM/zcm) found [here](https://github.com/ZeroCM/zcm/blob/master/docs/building.md).
+Remember to configure for Julia with either ./waf configure --use-julia to include Julia in the configuration or with ./waf configure --use-all to include all options.
+
+This is an unregistered package and can be added with:
+```julia
+(1.0) pkg> add https://github.com/AndreHat/ZCM.jl.git
+```
---
+++ example/basic_test.jl
@@ -0,0 +1,60 @@
+#pushfirst!(LOAD_PATH, "../build/types")
+#pushfirst!(LOAD_PATH, "../example")
+
+using ZCM
+
+include("../example/_example_t.jl")
+using Main._example_t
+
+numReceived = 0
+function handler(rbuf, channel::String, msg::example_t)
+    println("Received message on channel: ", channel)
+    global numReceived
+    @assert (numReceived == msg.timestamp) "Received message with incorrect timestamp"
+    numReceived = numReceived + 1
+end
+
+# a handler that receives the raw message bytes
+function untyped_handler(rbuf, channel::String, msgdata::Vector{UInt8})
+    println("Recieved raw message data on channel: ", channel)
+    decode(example_t, msgdata)
+end
+
+#zcm = Zcm("inproc")
+zcm = Zcm("udpm://239.255.76.67:7667?ttl=1")
+if (!good(zcm))
+    error("Unable to initialize zcm");
+end
+
+sub = subscribe(zcm, "EXAMPLE", handler, example_t)
+sub2 = subscribe(zcm, "EXAMPLE", untyped_handler)
+
+msg = example_t()
+
+ZCM.start(zcm)
+
+msg.timestamp = 0;
+publish(zcm, "EXAMPLE", msg)
+msg.timestamp = 1;
+publish(zcm, "EXAMPLE", msg)
+msg.timestamp = 2;
+publish(zcm, "EXAMPLE", msg)
+
+sleep(0.5)
+
+msg.timestamp = 3;
+publish(zcm, "EXAMPLE", msg)
+msg.timestamp = 4;
+publish(zcm, "EXAMPLE", msg)
+msg.timestamp = 5;
+publish(zcm, "EXAMPLE", msg)
+
+sleep(0.5)
+stop(zcm)
+
+unsubscribe(zcm, sub)
+unsubscribe(zcm, sub2)
+
+@assert (numReceived == 6) "Didn't receive proper number of messages"
+
+println("Success!")
